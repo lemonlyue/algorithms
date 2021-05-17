59b456e2bfecc8daf643a31b08e57bfe28299585
lemon_lyue <lemon_lyue@163.com>
2021-05-17 21:35:10 +0800

    Update LettCode 215.数组中的第K个最大元素 快速排序&大顶堆 https://leetcode-cn.com/problems/kth-largest-element-in-an-array/

--- src/LeetCode/215.php
+++ src/LeetCode/215.php
@@ -1,21 +1,128 @@
 <?php
+
+/**
+ * Class Solution
+ * @link https://leetcode-cn.com/problems/kth-largest-element-in-an-array/submissions/
+ */
 class Solution
 {
     function findKthLargest($nums, $k)
     {
-        $n = count($nums);
+        // 大顶堆
         $heap = new SplMinHeap();
-        for ($i = 0; $i < $n; ++$i) {
+        foreach ($nums as $iValue) {
             if ($heap->count() < $k) {
-                $heap->insert($nums[$i]);
-            } elseif ($heap->top() < $nums[$i]) {
+                $heap->insert($iValue);
+            } elseif ($heap->top() < $iValue) {
                 $heap->extract();
-                $heap->insert($nums[$i]);
+                $heap->insert($iValue);
             }
         }
-        var_dump($heap);
 
         return $heap->top();
+    }
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $k
+     * @return Integer
+     */
+    function findKthLargest2($nums, $k) {
+        $count = count($nums);
+        // 快速排序
+        $nums = $this->quickSort($nums, 0, $count - 1);
+        return $nums[$count - $k];
+    }
+
+    /**
+     * 快速排序
+     *
+     * @param array $array
+     * @param int $left
+     * @param int $right
+     * @return mixed
+     */
+    function quickSort(array &$array, int $left, int $right): array
+    {
+        if ($left < $right) {
+            $pivot = $this->partition($array, $left, $right);
+            $this->quickSort($array, $left, $pivot - 1);
+            $this->quickSort($array, $pivot + 1, $right);
+        }
+        return $array;
+    }
+
+    /**
+     * 分区, 返回基准值
+     *
+     * @param array $array
+     * @param int $left
+     * @param int $right
+     * @return int|mixed
+     */
+    private function partition(array &$array, int $left, int $right)
+    {
+        if ($left === $right) {
+            return null;
+        }
+        $this->dealPivot($array, $left, $right);
+        $compare = $array[$left];
+        while ($left !== $right) {
+            // 判断右边是否小于基准, 小于基准时赋值到左边
+            while ($right && $right !== $left) {
+                if ($compare > $array[$right]) {
+                    $array[$left++] = $array[$right];
+                    break;
+                }
+                $right--;
+            }
+
+            // 判断左边是否大于基准, 大于基准时赋值到右边
+            while ($left && $left !== $right) {
+                if ($array[$left] > $compare) {
+                    $array[$right--] = $array[$left];
+                    break;
+                }
+                $left++;
+            }
+        }
+        // 基准赋值到替换位置
+        $array[$right] = $compare;
+        return $right;
+    }
+
+    /**
+     * 基准取中位数 三数取中
+     *
+     * @param array $array
+     * @param int $left
+     * @param int $right
+     */
+    private function dealPivot(array &$array, int $left, int $right)
+    {
+        $middle = ($left + $right) / 2;
+        if (($array[$left] - $array[$middle]) * ($array[$middle] - $array[$right]) > 0) {
+            $this->swap($array, $left, $middle);
+            return ;
+        }
+        if (($array[$middle] - $array[$left]) * ($array[$left] - $array[$right]) > 0) {
+            return ;
+        }
+        $this->swap($array, $left, $right);
+    }
+
+    /**
+     * 交换
+     *
+     * @param array $array
+     * @param int $left
+     * @param int $right
+     */
+    private function swap(array &$array, int $left, int $right)
+    {
+        $temp = $array[$left];
+        $array[$left] = $array[$right];
+        $array[$right] = $temp;
     }
 }
 
