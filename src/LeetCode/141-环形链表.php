0b2f6adc0791379bdb8e0dd10ebfc526d662559f
lemon_lyue <lemon_lyue@163.com>
2021-06-03 23:13:41 +0800

    feat: 🎸 LeetCode 141.环形链表
    https://leetcode-cn.com/problems/linked-list-cycle/

---
+++ src/LeetCode/141-环形链表.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }
+ */
+
+/**
+ * Class Solution
+ * @link https://leetcode-cn.com/problems/linked-list-cycle/
+ */
+class Solution {
+
+    /**
+     * 哈希表
+     *
+     * @param ListNode $head
+     * @return Boolean
+     */
+    function hasCycle($head) {
+        $hash = [];
+        while ($head) {
+            if (in_array($head, $hash, true)) {
+                return true;
+            }
+            $hash[] = $head;
+            $head = $head->next;
+        }
+        return false;
+    }
+}
