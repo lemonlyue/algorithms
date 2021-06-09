0443f5a5a9705866a375780c21e0e20df396d81f
lemon_lyue <lemon_lyue@163.com>
2021-06-09 21:45:00 +0800

    feat: 🎸 LeetCode 34.在排序数组中查找元素的第一个和最后一个位置
    https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/

---
+++ src/LeetCode/34-在排序数组中查找元素的第一个和最后一个位置.php
@@ -0,0 +1,43 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function searchRange($nums, $target) {
+        $count = count($nums);
+        $left = 0;
+        $right = $count - 1;
+        $leftValue = -1;
+        $rightValue = -1;
+        while ($left <= $right) {
+            $middle = $left + floor(($right - $left) / 2);
+            if ($nums[$middle] > $target) {
+                $right = $middle - 1;
+            } elseif ($nums[$middle] < $target) {
+                $left = $middle + 1;
+            } else {
+                $left = $middle;
+                $right = $middle;
+                while (($leftBool = $nums[$left] === $target) || ($rightBool = $nums[$right] === $target)) {
+                    if ($leftBool) {
+                        $leftValue = $left;
+                        $left--;
+                    }
+                    if ($rightBool) {
+                        $rightValue = $right;
+                        $right++;
+                    }
+                }
+                return [
+                    $leftValue, $rightValue
+                ];
+            }
+        }
+        return [
+            $leftValue, $rightValue
+        ];
+    }
+}
