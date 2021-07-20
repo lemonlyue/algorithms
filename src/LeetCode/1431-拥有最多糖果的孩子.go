fd0e7d13b0af8c505c7ddaea4bb834b7265f6ce7
lemon_lyue <lemon_lyue@163.com>
2021-07-20 22:14:54 +0800

    feat: 🎸 LeetCode 1431-拥有最多糖果的孩子
    https://leetcode-cn.com/problems/kids-with-the-greatest-number-of-candies/

--- src/LeetCode/1431-拥有最多糖果的孩子.go
+++ src/LeetCode/1431-拥有最多糖果的孩子.go
@@ -1,1 +1,26 @@
-package LeetCode
+// @link https://leetcode-cn.com/problems/kids-with-the-greatest-number-of-candies/
+func kidsWithCandies(candies []int, extraCandies int) []bool {
+	count := len(candies)
+	var result = make([]bool, count)
+	maxValue := max(candies)
+	for i := 0; i < count; i++ {
+		value := candies[i] + extraCandies
+		if maxValue > value {
+			result[i] = false
+		} else {
+			result[i] = true
+		}
+	}
+	return result
+}
+
+// 求数组最大值
+func max(data []int) int {
+	var max = data[0]
+	for _, val := range data {
+		if max <= val {
+			max = val
+		}
+	}
+	return max
+}
