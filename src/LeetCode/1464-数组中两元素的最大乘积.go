3a3e7f9cd462bb4e7e4ff7d68073c28efa918ddc
lemon_lyue <lemon_lyue@163.com>
2021-07-21 21:44:37 +0800

    feat: 🎸 LeetCode 1464.数组中两元素的最大乘积
    https://leetcode-cn.com/problems/maximum-product-of-two-elements-in-an-array/

--- src/LeetCode/1464-数组中两元素的最大乘积.go
+++ src/LeetCode/1464-数组中两元素的最大乘积.go
@@ -1,1 +1,14 @@
-package LeetCode
+// @link https://leetcode-cn.com/problems/maximum-product-of-two-elements-in-an-array/
+func maxProduct(nums []int) int {
+	var max = 0
+	var second = 0
+	for _, value := range nums {
+		if max < value {
+			second = max
+			max = value
+		} else if second < value {
+			second = value
+		}
+	}
+	return (max - 1) * (second - 1)
+}
